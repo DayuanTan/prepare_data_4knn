e6c77c26567b4d45eeda8c8267cf2aa0531dad84
Dayuan•Neil Tan <dyt@DayuanNeils-MacBook-Pro.local>
2018-04-19 17:18:48 -0400

    add randomly choose company

--- prepare_9days_data/gene_8changes.py
+++ prepare_9days_data/gene_8changes.py
@@ -1,6 +1,6 @@
 import os
 
-resultHandler = open('9days_8changes_4predict.csv','w')
+resultHandler = open('9days_8changes_4predict_partial_companies.csv','w')
 
 path = "9days_dataset"
 files = os.listdir(path)
@@ -9,69 +9,81 @@ files.sort()
 #files.remove(".DS_Store")
 print(files)
 
+#get all compnies' names we randomly selected in "process_data_4knn[_do].py"
+companiesNamesRSltedHandler = open('randomSelectedCompanies.csv','r')
+companies_iter_f = iter(companiesNamesRSltedHandler)
+for companiesNames in companies_iter_f: 
+    companiesNames = companiesNames.rstrip()
+    print(companiesNames)
+    companiesNameArray = companiesNames.split(',')
+    print(companiesNameArray)
+  
+
 for file in files:
     if not os.path.isdir(file):         # only open if it's directory
-        f = open(path + "/" + file)     # open this file
-        iter_f = iter(f)                # create iterator
+        if file in companiesNameArray:            # only use this company if it's in the randomely selected companies
+            print("processing ",file)
+            f = open(path + "/" + file)     # open this file
+            iter_f = iter(f)                # create iterator
 
-        counter = 0
-        openChangesArray = list()
-        highChangesArray = list()
-        lowChangesArray = list()
-        closeChangesArray = list()
-        volumeChangesArray = list()
-        lastline = 'Hello,This is virtual empty line.'
-        for line in iter_f:             # traverse the file line by line
-            line = line.rstrip()
-            #print(line)
-            thisline = line.split(',')
-            #print(thisline[1]," ",lastline[1])
-            try:
-                openChanges = round(float(thisline[1]) - float(lastline[1]),2)
-                highChanges = round(float(thisline[2]) - float(lastline[2]), 2)
-                lowChanges = round(float(thisline[3]) - float(lastline[3]), 2)
-                closeChanges = round(float(thisline[4]) - float(lastline[4]), 2)
-                volumnChanges = int(thisline[5]) - int(lastline[5])
-                counter = counter + 1
-                #if counter <= 9:
-                if counter <= 8:
-                    openChangesArray.append(openChanges)
-                    highChangesArray.append(highChanges)
-                    lowChangesArray.append(lowChanges)
-                    closeChangesArray.append(closeChanges)
-                    volumeChangesArray.append(volumnChanges)
+            counter = 0
+            openChangesArray = list()
+            highChangesArray = list()
+            lowChangesArray = list()
+            closeChangesArray = list()
+            volumeChangesArray = list()
+            lastline = 'Hello,This is virtual empty line.'
+            for line in iter_f:             # traverse the file line by line
+                line = line.rstrip()
+                #print(line)
+                thisline = line.split(',')
+                #print(thisline[1]," ",lastline[1])
+                try:
+                    openChanges = round(float(thisline[1]) - float(lastline[1]),2)
+                    highChanges = round(float(thisline[2]) - float(lastline[2]), 2)
+                    lowChanges = round(float(thisline[3]) - float(lastline[3]), 2)
+                    closeChanges = round(float(thisline[4]) - float(lastline[4]), 2)
+                    volumnChanges = int(thisline[5]) - int(lastline[5])
+                    counter = counter + 1
+                    #if counter <= 9:
+                    if counter <= 8:
+                        openChangesArray.append(openChanges)
+                        highChangesArray.append(highChanges)
+                        lowChangesArray.append(lowChanges)
+                        closeChangesArray.append(closeChanges)
+                        volumeChangesArray.append(volumnChanges)
 
-            except:
-                print("Can't do divison.")
-            finally:
-                lastline = thisline
+                except:
+                    print("Can't do divison.")
+                finally:
+                    lastline = thisline
 
-            #print(openChangesArray)
+                #print(openChangesArray)
 
-        openChangesStr = str(openChangesArray)
-        openChangesStr = openChangesStr.lstrip('[')
-        openChangesStr = openChangesStr.rstrip(']')
-        highChangesStr = str(highChangesArray)
-        highChangesStr = highChangesStr.lstrip('[')
-        highChangesStr = highChangesStr.rstrip(']')
-        lowChangesStr = str(lowChangesArray)
-        lowChangesStr = lowChangesStr.lstrip('[')
-        lowChangesStr = lowChangesStr.rstrip(']')
-        closeChangesStr = str(closeChangesArray)
-        closeChangesStr = closeChangesStr.lstrip('[')
-        closeChangesStr = closeChangesStr.rstrip(']')
-        volumeChangesStr = str(volumeChangesArray)
-        volumeChangesStr = volumeChangesStr.lstrip('[')
-        volumeChangesStr = volumeChangesStr.rstrip(']')
+            openChangesStr = str(openChangesArray)
+            openChangesStr = openChangesStr.lstrip('[')
+            openChangesStr = openChangesStr.rstrip(']')
+            highChangesStr = str(highChangesArray)
+            highChangesStr = highChangesStr.lstrip('[')
+            highChangesStr = highChangesStr.rstrip(']')
+            lowChangesStr = str(lowChangesArray)
+            lowChangesStr = lowChangesStr.lstrip('[')
+            lowChangesStr = lowChangesStr.rstrip(']')
+            closeChangesStr = str(closeChangesArray)
+            closeChangesStr = closeChangesStr.lstrip('[')
+            closeChangesStr = closeChangesStr.rstrip(']')
+            volumeChangesStr = str(volumeChangesArray)
+            volumeChangesStr = volumeChangesStr.lstrip('[')
+            volumeChangesStr = volumeChangesStr.rstrip(']')
 
-        resultHandler.writelines(
-            [openChangesStr, ';', highChangesStr, ';', lowChangesStr, ';', closeChangesStr, ';', volumeChangesStr, ';',
-             thisline[6], ';',  thisline[1], ',', thisline[2], ',', thisline[3], ',', thisline[4], ',', thisline[5], '\n'])#also add 9th day's price
-        openChangesArray = list()
-        highChangesArray = list()
-        lowChangesArray = list()
-        closeChangesArray = list()
-        volumeChangesArray = list()
-        counter = 0
+            resultHandler.writelines(
+                [openChangesStr, ';', highChangesStr, ';', lowChangesStr, ';', closeChangesStr, ';', volumeChangesStr, ';',
+                thisline[6], ';',  thisline[1], ',', thisline[2], ',', thisline[3], ',', thisline[4], ',', thisline[5], '\n'])#also add 9th day's price
+            openChangesArray = list()
+            highChangesArray = list()
+            lowChangesArray = list()
+            closeChangesArray = list()
+            volumeChangesArray = list()
+            counter = 0
 
 
--- test/process_data_4knn.py
+++ test/process_data_4knn.py
@@ -4,8 +4,10 @@
 # and put those changes into a new file. Which will be used for KNN algorithm (which is not showed int this programme).
 
 import os
+import random
 
-resultHandler = open('results.csv','w') # assign a file to record the result
+resultHandler = open('results2.csv','w') # assign a file to record the result
+randomSelectedCompaniesHandler = open('randomSelectedCompanies.csv','w') # used to store names of randomly selected companies
 
 path = "sample"
 files = os.listdir(path)
@@ -14,68 +16,77 @@ files.sort()                            # sort it only for me to check it easily
 #files.remove(".DS_Store")               # only Mac OS needs this. I found Mac always creates this file for a directory.
 print(files)
 
+
 for file in files:
-    if not os.path.isdir(file):         # only open if it's directory
-        f = open(path + "/" + file)     # open this file
-        iter_f = iter(f)                # create iterator
+    # randomly choice compines
+    whetherTakeThisCompany = random.random()
+    if whetherTakeThisCompany >=0.5:
+        print(file, " discarded.")
+        continue
+    else:
+        print(file, " selected.")
+        randomSelectedCompaniesHandler.writelines([file,',']) # record the selected companies' names for 'prepare_9days_data' to use
+        if not os.path.isdir(file):         # only open if it's directory
+            f = open(path + "/" + file)     # open this file
+            iter_f = iter(f)                # create iterator
 
-        counter = 0
-        openChangesArray = list()
-        highChangesArray = list()
-        lowChangesArray = list()
-        closeChangesArray = list()
-        volumeChangesArray = list()
-        lastline = 'Hello,This is virtual empty line.'
-        for line in iter_f:             # traverse the file line by line
-            line = line.rstrip()
-            print(line)
-            thisline = line.split(',')
-            print(thisline[1]," ",lastline[1])
-            try:
-                openChanges = round(float(thisline[1]) - float(lastline[1]),2)
-                highChanges = round(float(thisline[2]) - float(lastline[2]), 2)
-                lowChanges = round(float(thisline[3]) - float(lastline[3]), 2)
-                closeChanges = round(float(thisline[4]) - float(lastline[4]), 2)
-                volumnChanges = round(float(thisline[5]) - float(lastline[5]), 2)
-                counter = counter + 1
-                if counter <= 9:
-                    openChangesArray.append(openChanges)
-                    highChangesArray.append(highChanges)
-                    lowChangesArray.append(lowChanges)
-                    closeChangesArray.append(closeChanges)
-                    volumeChangesArray.append(volumnChanges)
-                else:
-                    openChangesStr = str(openChangesArray)
-                    openChangesStr = openChangesStr.lstrip('[')
-                    openChangesStr = openChangesStr.rstrip(']')
-                    highChangesStr = str(highChangesArray)
-                    highChangesStr = highChangesStr.lstrip('[')
-                    highChangesStr = highChangesStr.rstrip(']')
-                    lowChangesStr = str(lowChangesArray)
-                    lowChangesStr = lowChangesStr.lstrip('[')
-                    lowChangesStr = lowChangesStr.rstrip(']')
-                    closeChangesStr = str(closeChangesArray)
-                    closeChangesStr = closeChangesStr.lstrip('[')
-                    closeChangesStr = closeChangesStr.rstrip(']')
-                    volumeChangesStr = str(volumeChangesArray)
-                    volumeChangesStr = volumeChangesStr.lstrip('[')
-                    volumeChangesStr = volumeChangesStr.rstrip(']')
+            counter = 0
+            openChangesArray = list()
+            highChangesArray = list()
+            lowChangesArray = list()
+            closeChangesArray = list()
+            volumeChangesArray = list()
+            lastline = 'Hello,This is virtual empty line.'
+            for line in iter_f:             # traverse the file line by line
+                line = line.rstrip()
+                print(line)
+                thisline = line.split(',')
+                print(thisline[1]," ",lastline[1])
+                try:
+                    openChanges = round(float(thisline[1]) - float(lastline[1]),2)
+                    highChanges = round(float(thisline[2]) - float(lastline[2]), 2)
+                    lowChanges = round(float(thisline[3]) - float(lastline[3]), 2)
+                    closeChanges = round(float(thisline[4]) - float(lastline[4]), 2)
+                    volumnChanges = round(float(thisline[5]) - float(lastline[5]), 2)
+                    counter = counter + 1
+                    if counter <= 9:
+                        openChangesArray.append(openChanges)
+                        highChangesArray.append(highChanges)
+                        lowChangesArray.append(lowChanges)
+                        closeChangesArray.append(closeChanges)
+                        volumeChangesArray.append(volumnChanges)
+                    else:
+                        openChangesStr = str(openChangesArray)
+                        openChangesStr = openChangesStr.lstrip('[')
+                        openChangesStr = openChangesStr.rstrip(']')
+                        highChangesStr = str(highChangesArray)
+                        highChangesStr = highChangesStr.lstrip('[')
+                        highChangesStr = highChangesStr.rstrip(']')
+                        lowChangesStr = str(lowChangesArray)
+                        lowChangesStr = lowChangesStr.lstrip('[')
+                        lowChangesStr = lowChangesStr.rstrip(']')
+                        closeChangesStr = str(closeChangesArray)
+                        closeChangesStr = closeChangesStr.lstrip('[')
+                        closeChangesStr = closeChangesStr.rstrip(']')
+                        volumeChangesStr = str(volumeChangesArray)
+                        volumeChangesStr = volumeChangesStr.lstrip('[')
+                        volumeChangesStr = volumeChangesStr.rstrip(']')
 
-                    resultHandler.writelines([openChangesStr,';',highChangesStr,';',lowChangesStr,';',closeChangesStr,';',volumeChangesStr,';',thisline[6],'\n'])
-                    openChangesArray = list()
-                    highChangesArray = list()
-                    lowChangesArray = list()
-                    closeChangesArray = list()
-                    volumeChangesArray = list()
-                    counter = 0
-                print(counter," ",openChangesArray)
-                #print(changesArray)
-            except:
-                print("Can't do divison.")
-            finally:
-                lastline = thisline
+                        resultHandler.writelines([openChangesStr,';',highChangesStr,';',lowChangesStr,';',closeChangesStr,';',volumeChangesStr,';',thisline[6],'\n'])
+                        openChangesArray = list()
+                        highChangesArray = list()
+                        lowChangesArray = list()
+                        closeChangesArray = list()
+                        volumeChangesArray = list()
+                        counter = 0
+                    print(counter," ",openChangesArray)
+                    #print(changesArray)
+                except:
+                    print("Can't do divison.")
+                finally:
+                    lastline = thisline
 
-            print(openChangesArray)
+                print(openChangesArray)
 
 
 
